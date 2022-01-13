54dfea10052beee5337a2f9e319621767ce6b78c
Nikita <saininikita23@gmail.com>
2022-01-13 21:44:08 +0530

    commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songReducer = () => {
     return [
         {title: 'Hard on Yourself', duration: '2:40'},
@@ -16,3 +18,8 @@ const selectedSongReducer = (selectedSong=null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers ({
+    songs: songReducer,
+    selectedSong: selectedSongReducer
+});
